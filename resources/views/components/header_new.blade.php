81af6a7a14e26aa4ff5e18c88e4ed427070fb5f1
Sumit Sharma <211.sumit@gmail.com>
2025-12-31 12:23:05 +0530

    static file and robots.txt fixes

--- resources/views/components/header_new.blade.php
+++ resources/views/components/header_new.blade.php
@@ -132,7 +132,7 @@
             <div class="dropdown d-inline-block">
                 <button type="button" class="btn header-item bg-light-subtle border-start border-end"
                     id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img class="rounded-circle header-profile-user" src="/assets/images/users/avatar-4.jpg"
+                    <img class="rounded-circle header-profile-user" src="/assets/images/users/avatar-7.jpg"
                         alt="Header Avatar">
                     <span class="d-none d-xl-inline-block ms-1 fw-medium">{{ auth()->user()->name }}</span>
                     <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -4,7 +4,7 @@
 <head>
 
     <meta charset="utf-8" />
-    <title>Jodi Search Dashboard</title>
+    <title>Elite Marriage Bureau - Dashboard</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesbrand" name="author" />
