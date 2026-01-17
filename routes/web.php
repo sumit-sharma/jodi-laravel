03edfdbcac8a181f9cc8c45399fbca6d167d9aeb
AIPS <aipsinstitute363@gmail.com>
2026-01-17 11:15:10 +0530

    delete copy

--- routes/web.php
+++ routes/web.php
@@ -78,8 +78,8 @@ Route::middleware("auth")->group(function () {
         Route::post('/store-occupation', [MasterController::class, 'storeOccupation'])->name('panel.store-occupation');
         Route::get('/get-active-employee', [MasterController::class, 'getActiveEmployee'])->name('panel.get-active-employee');
         Route::get('/change-password', [MasterController::class, 'changePassword'])->name('change-password');
-        Route::get('/link-tl-tc', [MasterController::class, 'linkTlTc'])->name('link-tl-tc');
         Route::post('/change-password', [MasterController::class, 'changePasswordStore'])->name('panel.change-password');
+        Route::get('/link-tl-tc', [MasterController::class, 'linkTlTc'])->name('link-tl-tc');
     });
 
     Route::resource('references', ReferenceController::class);
