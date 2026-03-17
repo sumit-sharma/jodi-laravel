cedbf99f5a9a88bdc5b7740ac74e1fffd001b251
Sumit Sharma <211.sumit@gmail.com>
2026-03-17 21:50:22 +0530

    fix: view more info as label

--- resources/views/panel/Customer/more_info/view_more_info.blade.php
+++ resources/views/panel/Customer/more_info/view_more_info.blade.php
@@ -27,11 +27,11 @@
 
                                     <div class="col-md-3 mt-3">
                                         <label for="" class="form-label">Reference No.</label>
-                                        <label class="form-control">{{ $rno }}</label>
+                                        <div class="label-form">{{ $rno }}</div>
                                     </div>
                                     <div class="col-md-3 mt-3">
                                         <label for="" class="form-label">Name </label>
-                                        <label class="form-control">{{ $bio->refname }}</label>
+                                        <div class="label-form">{{ $bio->refname }}</div>
                                     </div>
                                     <div class="col-md-3 mt-3">
                                         <label for="" class="form-label">Met With </label>
@@ -44,117 +44,79 @@
                                                 }
                                             }
                                         @endphp
-                                        <label class="form-control">{{ $name }}</label>
+                                        <div class="label-form">{{ $name }}</div>
                                     </div>
                                     <div class="clearfix"></div>
 
                                     <div class="col-md-4 mt-3">
                                         <label for="" class="form-label">Member</label>
-                                        <textarea name="member" class="form-control" rows="4" placeholder="Enter member"
-                                            spellcheck="false" disabled> {{ $moreInfo->member }}</textarea>
+                                        <div class="label-form">{{ $moreInfo->member }}</div>
                                     </div>
                                     <div class="col-md-4 mt-3">
                                         <label for="" class="form-label">Current Profession </label>
-                                        <textarea name="profession" class="form-control" rows="4"
-                                            placeholder="Enter current profession details" spellcheck="false"
-                                            disabled>{{ $moreInfo->profession }}</textarea>
+                                        <div class="label-form">{{ $moreInfo->profession }}</div>
                                     </div>
                                     <div class="col-md-4 mt-3">
                                         <label for="" class="form-label">Family Outlook </label>
-                                        <textarea name="family" class="form-control" rows="4"
-                                            placeholder="Enter family outlook" spellcheck="false"
-                                            disabled>{{ $moreInfo->family }}</textarea>
+                                        <div class="label-form">{{ $moreInfo->family }}</div>
                                     </div>
                                     <div class="clearfix"></div>
 
                                     <div class="col-md-3 mt-3">
                                         <label for="" class="form-label">Properties</label>
-                                        <select name="prop1" class="form-select" disabled>
-                                            <option value="" {{ $moreInfo->prop1 == '' ? 'selected' : '' }}>
-                                            </option>
-                                            <option value="Owned" {{ $moreInfo->prop1 == 'Owned' ? 'selected' : '' }}>
-                                                Owned</option>
-                                            <option value="Rented" {{ $moreInfo->prop1 == 'Rented' ? 'selected' : '' }}>
-                                                Rented</option>
-                                        </select>
+                                        <div class="label-form">{{ $moreInfo->prop1 }}</div>
                                     </div>
                                     <div class="col-md-3 mt-3">
                                         <label for="" class="form-label">&nbsp; </label>
-                                        <select name="prop2" class="form-select" disabled>
-                                            <option value="" {{ $moreInfo->prop2 == '' ? 'selected' : '' }}>
-                                            </option>
-                                            <option value="Kothi" {{ $moreInfo->prop2 == 'Kothi' ? 'selected' : '' }}>
-                                                Kothi</option>
-                                            <option value="Floor" {{ $moreInfo->prop2 == 'Floor' ? 'selected' : '' }}>
-                                                Floor</option>
-                                        </select>
+                                        <div class="label-form">{{ $moreInfo->prop2 }}</div>
                                     </div>
                                     <div class="col-md-3 mt-3">
                                         <label for="" class="form-label">&nbsp; </label>
-                                        <select name="prop3" class="form-select" disabled>
-                                            <option value="" {{ $moreInfo->prop3 == '' ? 'selected' : '' }}></option>
-                                            <option value="Joint" {{ $moreInfo->prop3 == 'Joint' ? 'selected' : '' }}>Joint
-                                            </option>
-                                            <option value="Nuclear" {{ $moreInfo->prop3 == 'Nuclear' ? 'selected' : '' }}>
-                                                Nuclear</option>
-                                            <option value="Floorwise" {{ $moreInfo->prop3 == 'Floorwise' ? 'selected' : '' }}>
-                                                Floorwise</option>
-                                        </select>
+                                        <div class="label-form">{{ $moreInfo->prop3 }}</div>
                                     </div>
                                     <div class="clearfix"></div>
 
                                     <div class="col-md-12 mt-3">
                                         <label for="" class="form-label">Properties Details </label>
-                                        <textarea name="properties" class="form-control" rows="4"
-                                            placeholder="Enter details" spellcheck="false"
-                                            disabled>{{ $moreInfo->properties }}</textarea>
+                                        <div class="label-form">{{ $moreInfo->properties }}</div>
                                     </div>
                                     <div class="clearfix"></div>
 
                                     <div class="col-md-4 mt-3">
                                         <label for="" class="form-label">Residence </label>
-                                        <textarea name="residence" class="form-control" rows="4" placeholder="Enter details"
-                                            spellcheck="false" disabled>{{ $moreInfo->residence }}</textarea>
+                                        <div class="label-form">{{ $moreInfo->residence }}</div>
                                     </div>
                                     <div class="col-md-4 mt-3">
                                         <label for="" class="form-label">Business </label>
-                                        <textarea name="business" class="form-control" rows="4" placeholder="Enter details"
-                                            spellcheck="false" disabled>{{ $moreInfo->business }}</textarea>
+                                        <div class="label-form">{{ $moreInfo->business }}</div>
                                     </div>
                                     <div class="col-md-4 mt-3">
                                         <label for="" class="form-label">Income </label>
-                                        <textarea name="income" class="form-control" rows="4" placeholder="Enter details"
-                                            spellcheck="false" disabled>{{ $moreInfo->income }}</textarea>
+                                        <div class="label-form">{{ $moreInfo->income }}</div>
                                     </div>
                                     <div class="clearfix"></div>
 
                                     <div class="col-md-4 mt-3">
                                         <label for="" class="form-label">Rented Income </label>
-                                        <textarea name="rentedincome" class="form-control" rows="4"
-                                            placeholder="Enter details" spellcheck="false"
-                                            disabled>{{ $moreInfo->rentedincome }}</textarea>
+                                        <div class="label-form">{{ $moreInfo->rentedincome }}</div>
                                     </div>
                                     <div class="col-md-4 mt-3">
                                         <label for="" class="form-label">Turnover </label>
-                                        <textarea name="turnover" class="form-control" rows="4" placeholder="Enter details"
-                                            spellcheck="false" disabled>{{ $moreInfo->turnover }}</textarea>
+                                        <div class="label-form">{{ $moreInfo->turnover }}</div>
                                     </div>
                                     <div class="col-md-4 mt-3">
                                         <label for="" class="form-label">Vehicle </label>
-                                        <textarea name="vehicle" class="form-control" rows="4" placeholder="Enter details"
-                                            spellcheck="false" disabled>{{ $moreInfo->vehicle }}</textarea>
+                                        <div class="label-form">{{ $moreInfo->vehicle }}</div>
                                     </div>
                                     <div class="clearfix"></div>
 
                                     <div class="col-md-6 mt-3">
                                         <label for="" class="form-label">Any Roka Earlier </label>
-                                        <textarea name="roka" class="form-control" rows="4" placeholder="Enter details"
-                                            spellcheck="false" disabled>{{ $moreInfo->roka }}</textarea>
+                                        <div class="label-form">{{ $moreInfo->roka }}</div>
                                     </div>
                                     <div class="col-md-6 mt-3">
                                         <label for="" class="form-label">Remarks </label>
-                                        <textarea name="remarks" class="form-control" rows="4" placeholder="Enter details"
-                                            spellcheck="false" disabled>{{ $moreInfo->remarks }}</textarea>
+                                        <div class="label-form">{{ $moreInfo->remarks }}</div>
                                     </div>
                                     <div class="clearfix"></div>
 
