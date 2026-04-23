9e5a15987e8e07ba09d46407f37107d18abc4318
Sumit Sharma <211.sumit@gmail.com>
2026-04-23 18:25:08 +0530

    fix: 503 template

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,133 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8" />
+    <title>Elite Marriage Bureau - Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesbrand" name="author" />
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="/assets/images/js-logo.webp">
+
+
+    <link rel="stylesheet" href="/assets/css/style2.css" type="text/css" />
+
+    <!-- twitter-bootstrap-wizard css -->
+    <link rel="stylesheet" href="/assets/libs/twitter-bootstrap-wizard/prettify.css">
+
+
+    <!-- plugin css -->
+    <link href="/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css" rel="stylesheet"
+        type="text/css" />
+
+    <!-- preloader css -->
+    <link rel="stylesheet" href="/assets/css/preloader.min.css" type="text/css" />
+
+    <!-- Bootstrap Css -->
+    <link href="/assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="/assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    @vite(['resources/js/app.js'])
+</head>
+
+
+<body data-topbar="dark">
+
+    <!-- <body data-layout="horizontal"> -->
+
+    <div class="bg-light-subtle min-vh-100 py-5">
+        <div class="py-4">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-center">
+                            <div class="row justify-content-center mb-5">
+                                <div class="col-sm-5">
+                                    <div class="maintenance-img">
+                                        <img src="assets/images/maintenance.png" alt=""
+                                            class="img-fluid mx-auto d-block">
+                                    </div>
+                                </div>
+                            </div>
+                            <h3 class="mt-4">Site is Under Maintenance</h3>
+                            <p>Please check back in sometime.</p>
+
+                            <!-- <div class="mt-4">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <div class="avatar-md mx-auto">
+                                                    <span class="avatar-title bg-primary-subtle rounded-circle">
+                                                        <i
+                                                            class="mdi mdi-access-point-network font-size-24 text-primary"></i>
+                                                    </span>
+                                                </div>
+
+                                                <h5 class="font-size-15 text-uppercase mt-4">Why is the Site Down?</h5>
+                                                <p class="text-muted mb-0">There are many variations of passages of
+                                                    Lorem Ipsum available, but the majority have suffered alteration.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <div class="avatar-md mx-auto">
+                                                    <span class="avatar-title bg-primary-subtle rounded-circle">
+                                                        <i class="mdi mdi-clock-outline font-size-24 text-primary"></i>
+                                                    </span>
+                                                </div>
+                                                <h5 class="font-size-15 text-uppercase mt-4">
+                                                    What is the Downtime?</h5>
+                                                <p class="text-muted mb-0">Contrary to popular belief, Lorem Ipsum is
+                                                    not
+                                                    simply random text. It has roots in a piece of classical.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <div class="avatar-md mx-auto">
+                                                    <span class="avatar-title bg-primary-subtle rounded-circle">
+                                                        <i class="mdi mdi-email-outline font-size-24 text-primary"></i>
+                                                    </span>
+                                                </div>
+                                                <h5 class="font-size-15 text-uppercase mt-4">
+                                                    Do you need Support?</h5>
+                                                <p class="text-muted mb-0">If you are going to use a passage of Lorem
+                                                    Ipsum, you need to be sure there isn't anything embar.. <a
+                                                        href="mailto:no-reply@domain.com"
+                                                        class="text-decoration-underline">no-reply@domain.com</a></p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                
+                        </div> -->
+                        </div>
+                    </div>
+                    <!-- end col -->
+                </div>
+                <!-- end row -->
+            </div>
+            <!-- end container -->
+        </div>
+    </div>
+    <!-- end  -->
+
+</body>
+
+</html>
